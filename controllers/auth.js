caf4f4db9025c0a3ee91fb11f883bc84e78f0908
Palathip Singchoo <palathip.opor@gmail.com>
2026-03-02 20:53:45 +0700

    Add Logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -107,3 +107,13 @@ exports.getMe = async (req, res, next) => {
   });
 };
 
+exports.logout=async(req,res,next)=>{
+    res.cookie('token','none',{
+    expires: new Date(Date.now()+ 10*1000),
+    httpOnly:true
+});
+    res.status(200).json({
+    success:true,
+    data:{}
+});
+};
